552c28956e98a9bb49f18e074dfb90bb9f1f3aed
Wizard-Of-Chaos <59537242+Wizard-Of-Chaos@users.noreply.github.com>
2022-03-31 17:23:28 -0400

    constant update for sensor interval

--- src/components/SensorComponent.h
+++ src/components/SensorComponent.h
@@ -11,7 +11,7 @@
 #include <vector>
 //Default range of a sensor
 #define DEFAULT_SENSOR_RANGE 500.f
-#define DEFAULT_SENSOR_UPDATE_INTERVAL 1.f
+#define DEFAULT_SENSOR_UPDATE_INTERVAL .4f
 /*
 * The sensor component allows a given entity to "see" what's around it. It looks for any Irrlicht node
 * within the radius (default 500), checks whether or not it's friendly, and then updates its list of contacts
