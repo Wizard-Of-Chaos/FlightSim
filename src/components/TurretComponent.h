dc68c079e8ba8b060b87d0fae7a5251b3cbf1c13
Wizard-Of-Chaos <59537242+Wizard-Of-Chaos@users.noreply.github.com>
2022-05-09 20:32:13 -0400

    Added turret component for carriers

---
+++ src/components/TurretComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef TURRETCOMPONENT_H
+#define TURRETCOMPONENT_H
+#include "BaseHeader.h"
+#include "ECS.h"
+
+const u32 MAX_TURRET_HARDPOINTS = 2;
+
+struct TurretComponent
+{
+	vector3df startingRotation; //this constrains how much the turret will be able to pitch/yaw
+	vector3df hardpoints[MAX_TURRET_HARDPOINTS];
+	EntityId weapons[MAX_TURRET_HARDPOINTS];
+	f32 pitchThrust;
+	f32 yawThrust;
+};
+
+#endif 
